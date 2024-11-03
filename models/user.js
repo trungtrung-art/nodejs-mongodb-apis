bd18bb12c8527fda5ce5f48ca2fed742a2d81304
Trần Trung <trantrung@192.168.102.10>
2024-11-04 00:30:34 +0700

    204. Adding Relational Order Data

--- models/user.js
+++ models/user.js
@@ -81,9 +81,18 @@ class User {
 
 	addOrder() {
 		const db = getDb()
-		return db
-			.collection('orders')
-			.insertOne(this.cart)
+		return this.getCart()
+			.then((products) => {
+				const order = {
+					items: products,
+					user: {
+						_od: new ObjectId(this._id),
+						name: this.name,
+						email: this.email,
+					},
+				}
+				return db.collection('orders').insertOne(order)
+			})
 			.then((result) => {
 				this.cart = { items: [] }
 				return db
@@ -93,6 +102,11 @@ class User {
 						{ $set: { cart: { items: [] } } },
 					)
 			})
+	}
+
+	getOrders() {
+		const db = getDb()
+		// return db.collection
 	}
 
 	static findById(userId) {
