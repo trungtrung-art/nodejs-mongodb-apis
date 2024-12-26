e95cce7b10c98526d9d86d19c176ffbe60c627c3
Trần Trung <trantrung@192.168.102.11>
2024-12-26 16:54:17 +0700

    245. Fixing Some Minor Bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 exports.getLogin = (req, res, next) => {
 	// const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1]
 	console.log(req.session.isLoggedIn)
@@ -10,6 +12,20 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-	req.session.isLoggedIn = true
-	res.redirect('/')
+	User.findById('673aae95a7d4df2fe7f4bf00')
+		.then((user) => {
+			req.session.isLoggedIn = true
+			req.session.user = user
+			res.redirect('/')
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+
+exports.postLogout = (req, res, next) => {
+	req.session.destroy((err) => {
+		console.log(err)
+		res.redirect('/')
+	})
 }
